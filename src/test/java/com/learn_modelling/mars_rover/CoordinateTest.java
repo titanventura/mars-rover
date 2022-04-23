7a5bdc159e48e593ca9db9e95f53070bf704f164
aswath.s <aswath.s@thoughtworks.com>
2022-04-23 12:18:17 +0530

    Add. Coordinate entity

---
+++ src/test/java/com/learn_modelling/mars_rover/CoordinateTest.java
@@ -0,0 +1,23 @@
+package com.learn_modelling.mars_rover;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CoordinateTest {
+    @Test
+    void shouldCompareOneCoordinateWithAnother() {
+        Coordinate coordinate = new Coordinate(2, 3);
+        Coordinate other = new Coordinate(2, 3);
+
+        assertTrue(coordinate.equals(other));
+    }
+
+    @Test
+    void shouldReturnOutOfBoundsWhenX2AndY2IsComparedWithX3AndY2() {
+        Coordinate coordinate = new Coordinate(2, 2);
+        Coordinate greater = new Coordinate(3,2);
+
+        assertTrue(greater.isOutOfBounds(coordinate));
+    }
+}
